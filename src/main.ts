ac95cd27459a9f2eb0ffa4f625a24bc01b162b20
pragyann007 <pragyanthapaliya027@gmail.com>
2026-09-24 17:59:10 +0545

    swagger setup..

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-
+import {SwaggerModule,DocumentBuilder } from "@nestjs/swagger"
 async function bootstrap() {
 
   const logger = new Logger("Bootstrap");
@@ -12,6 +12,10 @@ async function bootstrap() {
     transform:true
   }))
 
+  const config = new DocumentBuilder().setVersion("1.0.0").build();
+  const document = SwaggerModule.createDocument(app,config);
+
+  SwaggerModule.setup("api",app,document)
    
   await app.listen(process.env.PORT ?? 3000);
 }

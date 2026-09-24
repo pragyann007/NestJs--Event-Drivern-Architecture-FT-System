b3c845a55e1c9d01022d38e2fc3ff2e3e5b34d3a
pragyann007 <pragyanthapaliya027@gmail.com>
2026-09-24 18:25:30 +0545

    api docs updated..

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ async function bootstrap() {
     transform:true
   }))
 
-  const config = new DocumentBuilder().setVersion("1.0.0").build();
+  const config = new DocumentBuilder().setVersion("1.0.0")
+  .setTitle("Event Driven Architecture App").setDescription("This is the nest s backend api that gives the hands on expeience with the how the event driven system works how is the event being emited how persistance works etc etc ...").addServer("http://localhost:3000").setLicense("MIT","http://github.com/pragyann007").build();
   const document = SwaggerModule.createDocument(app,config);
 
   SwaggerModule.setup("api",app,document)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, DefaultValuePipe, Get, Param, Patch, Post, Query } from "@nestjs/common";
 import { UserDTO } from "./dtos/user.dto";
 import { PatchUserDto } from "./dtos/patch-user-dto";
 import { UsersService } from "./users.service";
+import { ApiParam, ApiProperty, ApiResponse } from "@nestjs/swagger"
 
 @Controller("users")
 export class UserController {
@@ -13,9 +14,25 @@ export class UserController {
         return `Hi ${userDetails.name} of ${userDetails.age}`;
     }
 
-    @Get()
-    getOneUser(){
-        return this.userservice.getOneUser("12anb")
+    @ApiProperty({
+        description:"This api returns the users with the user ifd that is sent from the params.."
+    })
+    @ApiParam({
+        name:"id",
+        example:"abc12ka",
+        description:"The valid user Id",
+        required:false
+    })
+    @ApiResponse({
+        status:200,
+        example:{
+            id:"abc12ka",
+            name:"Pragyan"
+        }
+    })
+    @Get("/:id")
+    getOneUser(@Param("id",new DefaultValuePipe("abcpragyan")) id:string){
+        return this.userservice.getOneUser(id)
     }
     @Patch()
     updateUser(@Body() userData: Partial<UserDTO>) {

931cfc87a91a57f42f378bf9691416855893c97d
pragyann007 <pragyanthapaliya027@gmail.com>
2026-09-26 06:22:07 +0545

    Document via JsDOc

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,23 @@ import { Injectable ,Inject,forwardRef} from '@nestjs/common';
 import { PostService } from 'src/posts/providers/post.service';
 
 @Injectable()
+/**
+ * Performs the business logic regarding Users  and interacts with user repiository 
+ */
 export class UsersService {
+    /** 
+     * This is the constructor that handles circular dependcy and injetc the postService */
     constructor(
         @Inject(forwardRef(()=>PostService))
         private readonly postService:PostService
     ){}
+    /**This method gets a user by its Id and returns only 1 user. */
     getOneUser(userId:string){
         return {id:userId,name:"Pragyan"}
     }
+    /**
+     * This method returns the total post  count of user created by this user of userId
+     */
     getUsersPostCount(postId:string){
         const name = this.postService.getPostName()
 
